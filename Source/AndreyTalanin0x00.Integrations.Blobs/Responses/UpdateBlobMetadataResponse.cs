7099c9d7f7d350f548feb4a6bdd6977d70aad896
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-22 22:06:33 +0100

    Add the UpdateBlobMetadataRequest, UpdateBlobMetadataResponse request and response classes

---
+++ Source/AndreyTalanin0x00.Integrations.Blobs/Responses/UpdateBlobMetadataResponse.cs
@@ -0,0 +1,8 @@
+using AndreyTalanin0x00.Integrations.Blobs.Requests;
+
+namespace AndreyTalanin0x00.Integrations.Blobs.Responses;
+
+public class UpdateBlobMetadataResponse
+{
+    public required UpdateBlobMetadataRequest Request { get; set; }
+}
