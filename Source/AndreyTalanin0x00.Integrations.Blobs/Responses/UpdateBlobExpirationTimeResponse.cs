3c40095f62318455c414b67e16896c770986a2d8
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-22 22:07:01 +0100

    Add the UpdateBlobExpirationTimeRequest, UpdateBlobExpirationTimeResponse request and response classes

---
+++ Source/AndreyTalanin0x00.Integrations.Blobs/Responses/UpdateBlobExpirationTimeResponse.cs
@@ -0,0 +1,8 @@
+using AndreyTalanin0x00.Integrations.Blobs.Requests;
+
+namespace AndreyTalanin0x00.Integrations.Blobs.Responses;
+
+public class UpdateBlobExpirationTimeResponse
+{
+    public required UpdateBlobExpirationTimeRequest Request { get; set; }
+}
